7c7869334f279bd05a588e50b770bfa2eb1933f6
Paolo Chang <pchang@architech.ca>
2022-04-22 14:20:51 -0400

    Pointing api_url to backend api endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,5 @@
 
 export const environment = {
   production: false,
-  // api_url: 'https://conduit.productionready.io/api'
-  api_url: '/api'
+  api_url: "https://second-springboot-rachdemo.azurewebsites.net/api",
 };
