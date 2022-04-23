eb7eb4d7aa0c0125e6a5635a15cae6763797e7de
Paolo Chang <pchang@architech.ca>
2022-04-22 23:39:19 -0400

    Correct the api_url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  api_url: "https://springboot-essentials-demo.azurewebsites.net/api",
+  api_url: "https://springboot-essentials-demo.azurewebsites.net/",
 };
